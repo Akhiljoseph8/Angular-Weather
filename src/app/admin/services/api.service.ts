07ed2ad575ca52d9b588715d59596045f52f63bb
Akhil Joseph <120910561+Akhiljoseph8@users.noreply.github.com>
2024-08-09 02:17:51 +0530

    Update api.service.ts

--- src/app/admin/services/api.service.ts
+++ src/app/admin/services/api.service.ts
@@ -4,7 +4,7 @@ import {HttpClient} from '@angular/common/http'
   providedIn: 'root'
 })
 export class ApiService {
-server_url:string="http://localhost:3000"
+server_url:string="https://weather-json-server-1.onrender.com"
   constructor(private http:HttpClient) { }
   addLocation(data:any){
     return this.http.post(`${this.server_url}/locations`,data)
